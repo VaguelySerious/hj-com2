a77c40dc7a1bca4e4c24990bc9a0a783f1c28bdb
Peter Wielander <peter@minervaproject.com>
2024-02-05 15:08:52 +0100

    Change email

--- assets/locales.js
+++ assets/locales.js
@@ -1,131 +1,131 @@
 export default {
   de: {
     meta: {
-      title: 'Rechtsanwaltskanzlei Herbst Johanna - Neumarkt (Bozen) Südtirol',
+      title: "Rechtsanwaltskanzlei Herbst Johanna - Neumarkt (Bozen) Südtirol",
       description:
-        'Rechtsanwältin und Mediatorin - Kanzlei für Familienrecht (Scheidungen, Trennungen, Sorgerechtsverfahren), Zivilrecht (Forderungseintreibungen, Mietangelegenheiten, Erbrecht, Vertragswesen, Verkehrsunfälle, Dingliche Rechte etc.), Strafrecht',
+        "Rechtsanwältin und Mediatorin - Kanzlei für Familienrecht (Scheidungen, Trennungen, Sorgerechtsverfahren), Zivilrecht (Forderungseintreibungen, Mietangelegenheiten, Erbrecht, Vertragswesen, Verkehrsunfälle, Dingliche Rechte etc.), Strafrecht",
       keywords:
-        'Rechtsanwalt, Mediatorin, Kanzlei, Recht, Beratung, Anwalt, Rechtsanwälte, Anwälte, Rechtsberatung, Fachanwalt, Anwaltskanzlei, Familienrecht, Scheidungen, Strafrecht, Erbrecht, Zivilrecht, Verkehrsunfälle, Vertragswesen, Sorgerechtsverfahren, Vertragswesen, Schadenersatzforderungen, Teilungen',
+        "Rechtsanwalt, Mediatorin, Kanzlei, Recht, Beratung, Anwalt, Rechtsanwälte, Anwälte, Rechtsberatung, Fachanwalt, Anwaltskanzlei, Familienrecht, Scheidungen, Strafrecht, Erbrecht, Zivilrecht, Verkehrsunfälle, Vertragswesen, Sorgerechtsverfahren, Vertragswesen, Schadenersatzforderungen, Teilungen",
     },
     nav: {
-      title: 'Kanzlei Herbst',
+      title: "Kanzlei Herbst",
       sections: [
-        { id: 'kontakt', prefix: 'Kanzlei', name: 'Kontakt' },
-        { id: 'leistungen', prefix: 'Unsere', name: 'Leistungen' },
-        { id: 'qualifikationen', prefix: 'Unsere', name: 'Qualifikationen' },
-        { id: 'team', prefix: 'Unser', name: 'Team' },
+        { id: "kontakt", prefix: "Kanzlei", name: "Kontakt" },
+        { id: "leistungen", prefix: "Unsere", name: "Leistungen" },
+        { id: "qualifikationen", prefix: "Unsere", name: "Qualifikationen" },
+        { id: "team", prefix: "Unser", name: "Team" },
       ],
     },
     kontakt: {
-      title: 'Kontakt',
-      prefix: 'Anwaltskanzlei',
-      heading: 'RA Avv. Dr. Johanna Herbst',
+      title: "Kontakt",
+      prefix: "Anwaltskanzlei",
+      heading: "RA Avv. Dr. Johanna Herbst",
       list: [
         {
-          name: 'Adresse',
-          body: 'Bahnhofsstraße 10/1 <br /> 39044 Neumarkt (BZ) <br /> Italien',
+          name: "Adresse",
+          body: "Bahnhofsstraße 10/1 <br /> 39044 Neumarkt (BZ) <br /> Italien",
         },
-        { name: 'Telefon', body: '+39 0471 820 715' },
-        { name: 'Fax', body: '+39 0471 813 526' },
+        { name: "Telefon", body: "+39 0471 820 715" },
+        { name: "Fax", body: "+39 0471 813 526" },
         {
-          name: 'Email Kanzlei',
-          body: `<a href="mailto:herbst.kanzlei@rolmail.net" rel="herbst.kanzlei/rolmail.net" class="herbstmail">herbst.kanzlei@rolmail.net</a>`,
+          name: "Email Kanzlei",
+          body: `<a href="mailto:info@kanzlei-herbst.it" rel="info/kanzlei-herbst.it" class="herbstmail">info@kanzlei-herbst.it</a>`,
         },
         {
-          name: 'PEC Email Johanna Herbst',
+          name: "PEC Email Johanna Herbst",
           body: `<a href="mailto:johanna.herbst@pec.rolmail.net" rel="johanna.herbst/pec.rolmail.net" class="herbstmail"> johanna.herbst@pec.rolmail.net</a>`,
         },
-        { name: 'Mehrwertssteuer Nr.', body: '02605690219' },
-        { name: 'Steuernummer', body: 'HRBJNN73D56F950Z' },
+        { name: "Mehrwertssteuer Nr.", body: "02605690219" },
+        { name: "Steuernummer", body: "HRBJNN73D56F950Z" },
       ],
     },
     leistungen: {
-      title: 'Tätigkeitsbereiche',
+      title: "Tätigkeitsbereiche",
       left: {
         title:
-          'Unsere Kanzlei berät und unterstützt Sie gerne in allen Bereichen des Zivilrechtes:',
+          "Unsere Kanzlei berät und unterstützt Sie gerne in allen Bereichen des Zivilrechtes:",
         list: [
           {
-            big: 'Familienrecht ',
+            big: "Familienrecht ",
             small:
-              '(Ehetrennungen und Scheidungen, Unterhaltsforderungen, Sorgerechtsverfahren usw.)',
+              "(Ehetrennungen und Scheidungen, Unterhaltsforderungen, Sorgerechtsverfahren usw.)",
           },
           {
-            big: 'Dingliche Rechte ',
+            big: "Dingliche Rechte ",
             small:
-              '(Eigentum, Fruchtgenuss, Wohnrecht, Grenzstreitigkeiten, Besitzschutzverfahren, Eigentumsfreiheitsklagen, Teilungen usw.)',
+              "(Eigentum, Fruchtgenuss, Wohnrecht, Grenzstreitigkeiten, Besitzschutzverfahren, Eigentumsfreiheitsklagen, Teilungen usw.)",
           },
-          { big: 'Forderungseintreibungen und Vollstreckungsverfahren' },
-          { big: 'Miet- und Kondominiumsangelegenheiten' },
-          { big: 'Erbrecht' },
-          { big: 'Vertragswesen' },
-          { big: 'Verkehrsunfälle' },
-          { big: 'Schadenersatzforderungen' },
-          { big: 'Sachwalterschaften' },
+          { big: "Forderungseintreibungen und Vollstreckungsverfahren" },
+          { big: "Miet- und Kondominiumsangelegenheiten" },
+          { big: "Erbrecht" },
+          { big: "Vertragswesen" },
+          { big: "Verkehrsunfälle" },
+          { big: "Schadenersatzforderungen" },
+          { big: "Sachwalterschaften" },
         ],
       },
       right: {
-        title: 'Im Strafrecht:',
+        title: "Im Strafrecht:",
         list: [
-          { big: 'Strafverfahren' },
-          { big: 'Amtsverteidigungen' },
-          { big: 'Einlassung als Zivilpartei im Strafverfahren' },
-          { big: 'Ableistung der Strafe durch Sozialdienst' },
-          { big: 'Strafzumessungen' },
+          { big: "Strafverfahren" },
+          { big: "Amtsverteidigungen" },
+          { big: "Einlassung als Zivilpartei im Strafverfahren" },
+          { big: "Ableistung der Strafe durch Sozialdienst" },
+          { big: "Strafzumessungen" },
         ],
       },
     },
     qualifikationen: {
-      title: 'Berufliche Qualifikationen',
+      title: "Berufliche Qualifikationen",
       left: {
         list: [
-          { small: 'Rechtsanwältin (zugelassen am obersten Gerichtshof)' },
+          { small: "Rechtsanwältin (zugelassen am obersten Gerichtshof)" },
           {
             small:
-              'Mediatorin (Mediationsstelle der Handelskammer Bozen und der Anwaltskammer Bozen)',
+              "Mediatorin (Mediationsstelle der Handelskammer Bozen und der Anwaltskammer Bozen)",
           },
           {
             small:
-              'Präsidentin AMI für die Provinz Bozen (Vereinigung der Italienischen Scheidungsanwälte)',
+              "Präsidentin AMI für die Provinz Bozen (Vereinigung der Italienischen Scheidungsanwälte)",
           },
           {
             small:
-              'Mitglied des Beirates für Chancengleichheit des Gerichtsrates des Oberlandesgerichtes Trient',
+              "Mitglied des Beirates für Chancengleichheit des Gerichtsrates des Oberlandesgerichtes Trient",
           },
           {
             small:
-              'Gerichtliche Verwahrerin und Verkaufsbeauftragte von gepfändeten Liegenschaften für das Landesgericht Bozen',
+              "Gerichtliche Verwahrerin und Verkaufsbeauftragte von gepfändeten Liegenschaften für das Landesgericht Bozen",
           },
         ],
       },
       right: {
         list: [
-          { small: 'Sachwalterin' },
-          { small: 'Amtsverteidigerin' },
+          { small: "Sachwalterin" },
+          { small: "Amtsverteidigerin" },
           {
             small:
-              'Zugelassene Rechtsanwältin für die Abwicklung der Verfahren mittels Prozesskostenbeihilfe',
+              "Zugelassene Rechtsanwältin für die Abwicklung der Verfahren mittels Prozesskostenbeihilfe",
           },
-          { small: 'Beratungstätigkeit für das Frauenbüro der Provinz Bozen' },
+          { small: "Beratungstätigkeit für das Frauenbüro der Provinz Bozen" },
         ],
       },
     },
     team: {
-      title: 'Unser Team',
+      title: "Unser Team",
       // main: {
       //   prefix: 'RA DR.',
       //   name: 'Johanna Herbst',
       //   role: 'Rechtsanwältin und Mediatorin',
       // },
       roles: {
-        RADR: 'RA DR.',
-        DR: 'DR.',
-        avvMed: 'Rechtsanwältin und Mediatorin',
-        jur: 'Juristin',
-        avv: 'Rechtsanwalt',
-        avv_in: 'Rechtsanwältin',
-        sek: 'Sekretärin',
-        anwärter: 'Rechtsanwaltsanwärterin',
+        RADR: "RA DR.",
+        DR: "DR.",
+        avvMed: "Rechtsanwältin und Mediatorin",
+        jur: "Juristin",
+        avv: "Rechtsanwalt",
+        avv_in: "Rechtsanwältin",
+        sek: "Sekretärin",
+        anwärter: "Rechtsanwaltsanwärterin",
       },
       list: [
         // {
@@ -148,95 +148,95 @@ export default {
   },
   it: {
     meta: {
-      title: 'Studio Legale Avvocato Herbst Johanna - Egna (Prov. Bolzano)',
+      title: "Studio Legale Avvocato Herbst Johanna - Egna (Prov. Bolzano)",
       description:
-        'Avvocato e mediatrice Johanna Herbst - Studio legale per diritto di famiglia (divorzi e separazioni, procedimenti di affidazione), diritto civile (recupero crediti, diritto di locazione, successioni, contratti, incidenti stradali, diritti reali etc.), diritto penale',
+        "Avvocato e mediatrice Johanna Herbst - Studio legale per diritto di famiglia (divorzi e separazioni, procedimenti di affidazione), diritto civile (recupero crediti, diritto di locazione, successioni, contratti, incidenti stradali, diritti reali etc.), diritto penale",
       keywords:
-        'legale, diritto, consulenza, avvocato, legali, studio legale, avvocati, consulenza legale, avvocato specialista, studio legale, divorzi, separazioni, diritto, civile, contratti, successioni, incidenti, penale',
+        "legale, diritto, consulenza, avvocato, legali, studio legale, avvocati, consulenza legale, avvocato specialista, studio legale, divorzi, separazioni, diritto, civile, contratti, successioni, incidenti, penale",
     },
     nav: {
-      title: 'Studio Legale Herbst',
+      title: "Studio Legale Herbst",
       sections: [
-        { id: 'kontakt', prefix: 'studio legale', name: 'Contatto' },
-        { id: 'leistungen', prefix: 'le nostre', name: 'Attivitá' },
-        { id: 'qualifikationen', prefix: 'le nostre', name: 'Qualifiche' },
-        { id: 'team', prefix: 'il nostro', name: 'Team' },
+        { id: "kontakt", prefix: "studio legale", name: "Contatto" },
+        { id: "leistungen", prefix: "le nostre", name: "Attivitá" },
+        { id: "qualifikationen", prefix: "le nostre", name: "Qualifiche" },
+        { id: "team", prefix: "il nostro", name: "Team" },
       ],
     },
     kontakt: {
-      title: 'Contatto',
-      prefix: 'Studio Legale',
-      heading: 'Avv. Dr. Johanna Herbst',
+      title: "Contatto",
+      prefix: "Studio Legale",
+      heading: "Avv. Dr. Johanna Herbst",
       list: [
         {
-          name: 'Indirizzo',
-          body: 'Via Stazione 10/1 <br />39044 Egna (BZ) <br />Italia',
+          name: "Indirizzo",
+          body: "Via Stazione 10/1 <br />39044 Egna (BZ) <br />Italia",
         },
-        { name: 'Telefono', body: '+39 0471 820 715' },
-        { name: 'Fax', body: '+39 0471 813 526' },
+        { name: "Telefono", body: "+39 0471 820 715" },
+        { name: "Fax", body: "+39 0471 813 526" },
         {
-          name: 'Email Studio Legale',
-          body: `<a href="mailto:herbst.kanzlei@rolmail.net" rel="herbst.kanzlei/rolmail.net" class="herbstmail">herbst.kanzlei@rolmail.net</a>`,
+          name: "Email Studio Legale",
+          body: `<a href="mailto:info@kanzlei-herbst.it" rel="info/kanzlei-herbst.it" class="herbstmail">info@kanzlei-herbst.it</a>`,
         },
         {
-          name: 'PEC Email Johanna Herbst',
+          name: "PEC Email Johanna Herbst",
           body: `<a href="mailto:johanna.herbst@pec.rolmail.net" rel="johanna.herbst/pec.rolmail.net" class="herbstmail"> johanna.herbst@pec.rolmail.net</a>`,
         },
-        { name: 'Partita Iva', body: '02605690219' },
-        { name: 'Codice Fiscale', body: 'HRBJNN73D56F950Z' },
+        { name: "Partita Iva", body: "02605690219" },
+        { name: "Codice Fiscale", body: "HRBJNN73D56F950Z" },
       ],
     },
     leistungen: {
-      title: 'Attivitá',
+      title: "Attivitá",
       left: {
         title:
-          'Il nostro studio saprà consigliarvi in tutti gli ambiti del diritto civile:',
+          "Il nostro studio saprà consigliarvi in tutti gli ambiti del diritto civile:",
         list: [
           {
-            big: 'Diritto di famiglia',
+            big: "Diritto di famiglia",
             small:
-              '(procedimenti di separazione e di divorzio, richieste di mantenimento, affidamento etc.)',
+              "(procedimenti di separazione e di divorzio, richieste di mantenimento, affidamento etc.)",
           },
           {
-            big: 'Diritti reali',
+            big: "Diritti reali",
             small:
-              '(proprietà, usufrutto, diritto di abitazione, usucapione, regolamento di confine, possessorie, tutela della proprietà, divisioni etc.)',
+              "(proprietà, usufrutto, diritto di abitazione, usucapione, regolamento di confine, possessorie, tutela della proprietà, divisioni etc.)",
           },
-          { big: 'Recupero di crediti e procedimenti esecutivi' },
-          { big: 'Locazioni e diritto condominiale' },
-          { big: 'Successioni' },
-          { big: 'Diritto contrattuale' },
-          { big: 'Incidenti stradali' },
-          { big: 'Risarcimento danni' },
-          { big: 'Amministrazioni di sostegno' },
+          { big: "Recupero di crediti e procedimenti esecutivi" },
+          { big: "Locazioni e diritto condominiale" },
+          { big: "Successioni" },
+          { big: "Diritto contrattuale" },
+          { big: "Incidenti stradali" },
+          { big: "Risarcimento danni" },
+          { big: "Amministrazioni di sostegno" },
         ],
       },
       right: {
-        title: 'Nel diritto penale:',
+        title: "Nel diritto penale:",
         list: [
-          { big: 'Procedimenti penali' },
+          { big: "Procedimenti penali" },
           { big: "Difese d'ufficio" },
-          { big: 'Costituzioni di parte civile nel procedimento penale' },
-          { big: 'Richieste di messa alla prova e lavoro socialmente utile' },
+          { big: "Costituzioni di parte civile nel procedimento penale" },
+          { big: "Richieste di messa alla prova e lavoro socialmente utile" },
           {
-            big: 'Applicazione della pena su richiesta',
-            small: ' (patteggiamento)',
+            big: "Applicazione della pena su richiesta",
+            small: " (patteggiamento)",
           },
         ],
       },
     },
     qualifikationen: {
-      title: 'Qualifiche professionali',
+      title: "Qualifiche professionali",
       left: {
         list: [
-          { small: 'Avvocata (patrocinante in Cassazione)' },
+          { small: "Avvocata (patrocinante in Cassazione)" },
           {
             small:
               "Mediatrice (iscritta presso l'organismo di mediazione della Camera di Commercio di Bolzano e presso l'organsimo di mediazione dell'Ordine degli Avvocati)",
           },
           {
             small:
-              'Presidente AMI per la Provincia di Bolzano (Associazione Avvocati Matrimonialisti Italiani)',
+              "Presidente AMI per la Provincia di Bolzano (Associazione Avvocati Matrimonialisti Italiani)",
           },
           {
             small:
@@ -244,17 +244,17 @@ export default {
           },
           {
             small:
-              'Custode giudiziario e delegato alla vendita degli immobili pignorati per il Tribunale di Bolzano',
+              "Custode giudiziario e delegato alla vendita degli immobili pignorati per il Tribunale di Bolzano",
           },
         ],
       },
       right: {
         list: [
-          { small: 'Amministratrice di sostegno' },
+          { small: "Amministratrice di sostegno" },
           { small: "Difensore d'ufficio" },
           {
             small:
-              'Avvocata ammessa allo svolgimento di procedimenti con gratuito patrocinio',
+              "Avvocata ammessa allo svolgimento di procedimenti con gratuito patrocinio",
           },
           {
             small:
@@ -264,18 +264,18 @@ export default {
       },
     },
     team: {
-      title: 'Unser Team',
+      title: "Unser Team",
       roles: {
-        RADR: 'AVV.',
-        DR: 'DR.',
-        avvMed: 'Avvocato e mediatore',
-        jur: 'Giurista',
-        avv: 'Avvocato',
-        avv_in: 'Avvocato',
-        sek: 'Segretaria',
-        anwärter: 'Praticante Avvocato',
+        RADR: "AVV.",
+        DR: "DR.",
+        avvMed: "Avvocato e mediatore",
+        jur: "Giurista",
+        avv: "Avvocato",
+        avv_in: "Avvocato",
+        sek: "Segretaria",
+        anwärter: "Praticante Avvocato",
       },
       list: [],
     },
   },
-}
+};
