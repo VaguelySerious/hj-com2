27a44ffee4896eb0029ef0f522ccae32a2264ac5
Peter Wielander <peter@minervaproject.com>
2024-06-27 17:59:58 +0200

    Remove instant.page

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyHead extends Head {
   render() {
-    let { head } = this.context
-    console.log(this.context)
-    let children = this.props.children
+    let { head } = this.context;
+    console.log(this.context);
+    let children = this.props.children;
 
-    console.log({ children })
+    console.log({ children });
 
     // const cssFiles = (files || []).filter((file) => /\.css$/.test(file))
     return (
@@ -20,30 +20,30 @@ class MyHead extends Head {
         ))} */}
         {/* {this.getCssLinks()} */}
       </head>
-    )
+    );
   }
 }
 
 export default class extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
   render() {
-    const isDev = process.env.NODE_ENV === 'development'
+    const isDev = process.env.NODE_ENV === "development";
     return (
       <Html>
         {isDev ? <Head /> : <MyHead />}
         <body>
           <Main />
           {isDev && <NextScript />}
-          <script
+          {/* <script
             src="//instant.page/5.1.0"
             type="module"
             integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"
-          ></script>
+          ></script> */}
         </body>
       </Html>
-    )
+    );
   }
 }
